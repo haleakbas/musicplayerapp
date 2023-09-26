1a6e8b4420e589fc73aa352947f35135a0408b78
haleakbas <146046440+haleakbas@users.noreply.github.com>
2023-09-25 22:35:41 -0500

    This app was coded with JavaScript, CSS, and HTML. It is currently still under development. The user can skip through the song using the slider.

---
+++ Java/data.js
@@ -0,0 +1,17 @@
+let songs = [
+{
+    name: 'fall',
+    path: 'Jurrivh_Fall.mp3',
+    artist: 'Joni Van Rossum, Jurrivh',
+    cover: 'fall.jpg'
+},
+
+{
+    name: 'suzume',
+    path: 'suzume.mp3',
+    artist: 'RADWIMPS, Toaka',
+    cover: 'suzucover.jpg'
+
+}
+
+]
